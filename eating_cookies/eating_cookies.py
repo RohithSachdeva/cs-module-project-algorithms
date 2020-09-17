be724aa9e785588cd63a7ef3303bfc55b70e3397
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-09-17 15:02:54 -0700

    Few solutions.. Some of the ones I did were done in lecture today and replaced as I took notes this morning.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,33 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+#Replaced my ON^3 solution from support hours with lecture example this morning.
+
+def eating_cookies(n, cache=None):
+    # what are our base cases?
+    if n < 0:
+        return 0
+    # this represents there being a number of cookies where we can just take
+    # that many cookies
+    elif n == 0:
+        return 1
+    # check the cache to see if the answer is stored in there
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            # init an empty cache
+            cache = {i: 0 for i in range(n+1)}
+        # store answers in our cache
+        cache[n] = eating_cookies(
+            n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 7
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
